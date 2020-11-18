5eee9ff62993de6373a56991aaa322688d9207b5
Shintaro Sumi <>
2020-11-19 01:28:29 +0900

    レビュー一覧の作成

--- chapter9/vue-router-2/src/api/products.js
+++ chapter9/vue-router-2/src/api/products.js
@@ -3,19 +3,80 @@ const database = [
     id: 1,
     name: '商品A',
     price: 100,
-    content: '商品A詳細'
+    content: '商品A詳細',
+    reviews: [
+      {
+        id: 1,
+        reviewer: 'Aaron',
+        comment: '商品A良い',
+        evaluation: 5
+
+      },
+      {
+        id: 2,
+        reviewer: 'Cyrus',
+        comment: '商品A普通',
+        evaluation: 3
+      },
+      {
+        id: 3,
+        reviewer: 'Harold',
+        comment: '商品A良くない',
+        evaluation: 1
+      }
+    ]
   },
   {
     id: 2,
     name: '商品B',
     price: 200,
-    content: '商品B詳細'
+    content: '商品B詳細',
+    reviews: [
+      {
+        id: 1,
+        reviewer: 'Aaron',
+        comment: '商品B良い',
+        evaluation: 5
+      },
+      {
+        id: 2,
+        reviewer: 'Cyrus',
+        comment: '商品B普通',
+        evaluation: 3
+      },
+      {
+        id: 3,
+        reviewer: 'Harold',
+        comment: '商品B良くない',
+        evaluation: 1
+      }
+    ]
   },
   {
     id: 3,
     name: '商品C',
     price: 300,
-    content: '商品C詳細'
+    content: '商品C詳細',
+    reviews: [
+      {
+        id: 1,
+        reviewer: 'Aaron',
+        comment: '商品C良い',
+        evaluation: 5
+      },
+      {
+        id: 2,
+        reviewer: 'Cyrus',
+        comment: '商品C普通',
+        evaluation: 3
+      },
+      {
+        id: 3,
+        reviewer: 'Harold',
+        comment: '商品C良くない',
+        evaluation: 1
+      }
+    ]
   }
 ]
 
--- chapter9/vue-router-2/src/views/Product/Review.vue
+++ chapter9/vue-router-2/src/views/Product/Review.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="product-review">
-    <h2>商品ReviewList</h2>
+    <h2>レビュー一覧</h2>
+    <ul>
+      <li v-for="{ id, reviewer, evaluation} in detail.reviews" :key="id">
+        <div>
+          <div>{{ reviewer }}さんのレビュー</div>
+          <div>
+            <span v-for="n in evaluation" :key="n">★</span>
+          </div>
+          <div><router-link :to="`review/${id}`" exact>レビュー詳細</router-link></div>
+        </div>
+      </li>
+    </ul>
+    <router-link :to="{ name: 'product-home' }" exact>商品詳細へ</router-link>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapGetters } from 'vuex'
+export default{
+  computed: {
+    ...mapGetters('product', ['detail'])
+  }
+}
 </script>
