b5aabdcbd79fba1c12def90ed3a7cfe96ef7cefc
Shintaro Sumi <>
2020-12-05 00:23:02 +0900

    グローバルにフックを置いてみる

--- chapter9/vue-router-2/src/router.js
+++ chapter9/vue-router-2/src/router.js
@@ -57,4 +57,18 @@ const router = new VueRouter({
   ]
 })
 
+router.beforeEach((to, from, next) => {
+  console.log('global:beforeEach')
+  next()
+})
+
+router.beforeResolve((to, from, next) => {
+  console.log('global:beforeResolve')
+  next()
+})
+
+router.afterEach((to, from, next) => {
+  console.log('global:afterEach')
+})
+
 export default router
